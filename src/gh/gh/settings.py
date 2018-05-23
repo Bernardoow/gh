409cad0fcf5505ef88f9b2731f0de49f42c4b083
Bernardo Gomes <bgomesdeabreu@gmail.com>
2018-05-22 21:00:29 -0300

    Changed robottxt_obey to false

--- src/gh/gh/settings.py
+++ src/gh/gh/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'gh.spiders'
 #USER_AGENT = 'gh (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
