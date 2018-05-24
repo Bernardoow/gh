136cfafdba8e8635fad5bf63dcd7f3988a2df087
Bernardo Gomes <bgomesdeabreu@gmail.com>
2018-05-23 22:27:55 -0300

    Added validation to FileModel.

--- src/models.py
+++ src/models.py
@@ -4,12 +4,51 @@ import attr
 
 @attr.s
 class FileModel(object):
-    url = attr.ib()
-    qty_lines = attr.ib()
-    size_file = attr.ib()
-    unit = attr.ib()
-    is_file = attr.ib()
-    extensions_file_url = attr.ib()
+    url = attr.ib(type=str, converter=str)
+
+    @url.validator
+    def check(self, attribute, value):
+        if value is None:
+            raise ValueError(f"{attribute.name} must be string")
+
+    qty_lines = attr.ib(type=int, converter=int)
+
+    @qty_lines.validator
+    def check(self, attribute, value):
+        if value < 0:
+            raise ValueError(f"{attribute.name} must be bigger or equal to 0")
+
+    size_file = attr.ib(type=float, converter=float)
+
+    @size_file.validator
+    def check(self, attribute, value):
+        if value < 0:
+            raise ValueError(f"{attribute.name} must be bigger or equal to 0")
+
+    unit = attr.ib(type=str, converter=str)
+
+    @unit.validator
+    def check(self, attribute, value):
+        valids_values = ["Bytes", "KB", "MB"]
+        if value is None:
+            raise ValueError(f"{attribute.name} must be string")
+        elif value not in valids_values and value != '-':
+            msg = f"{attribute.name} must be {', '.join(valids_values)}"
+            raise ValueError(msg)
+
+    is_file = attr.ib(type=int, converter=int)
+
+    @is_file.validator
+    def check(self, attribute, value):
+        if value < 0 or value > 1:
+            raise ValueError(f"{attribute.name} must be 0 or 1")
+
+    extensions_file_url = attr.ib(type=str, converter=str)
+
+    @extensions_file_url.validator
+    def check(self, attribute, value):
+        if value is None:
+            raise ValueError(f"{attribute.name} must be string")
 
     def get_size_in_bytes(self):
         if self.unit == 'Bytes':
